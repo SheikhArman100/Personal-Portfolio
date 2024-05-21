7698a8ed008fce979502ba3e6fa18e6c149e36ee
SheikhArman100 <sheikharman100@gmail.com>
2024-05-21 13:39:32 +0600

    updated resume added

--- components/DownloadCV.jsx
+++ components/DownloadCV.jsx
@@ -10,7 +10,8 @@ import Link from 'next/link.js'
 
 const DownloadCV = () => {
   return (
-    <Link href='/sk_md_golam_arman_cv.pdf' download='sk_md_golam_arman_cv.pdf'>
+    <a href='/Sk.MD.Golam-Arman-Resume.pdf' download='Sk.MD.Golam-Arman-Resume.pdf' target='_blank'
+    rel='noopener noreferrer'>
       <motion.button
         variants={downloadButtonAnimation}
         className='group flex origin-left  items-center gap-x-1   border-b border-yellowColor  px-4 sm:px-3   md:px-5'
@@ -25,7 +26,7 @@ const DownloadCV = () => {
           <span className='inline-flex h-0 w-0 -rotate-180 transform items-center justify-center rounded-full bg-yellowColor transition-all duration-500 ease-in-out group-hover:rotate-0 group-hover:p-[0.8rem] group-hover:w-[0.9rem] group-hover:h-[0.9rem] overflow-hidden'><MoveRight className="size-4 stroke-white"/></span>
         </div>
       </motion.button>
-    </Link>
+    </a>
   )
 }
 
